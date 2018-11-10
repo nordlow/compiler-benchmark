57947d47668b19a05ad9e2f514f7127608da9827
Per Nordlöw <per.nordlow@gmail.com>
2018-11-10 16:31:06 +0100

    *** empty log message ***

---
+++ benchmark.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+import os.path
+
+
+def regenerate_sources(root_path='generated'):
+    lang = "d"
+    with open(os.path.join(root_path, lang, "foo." + lang), 'w') as f:
+        f.write('''#!/usr/bin/env rdmd
+
+long foo0(long x)
+{
+    return x * 0;
+}
+
+void main(string[] args)
+{
+    long sum = 0;
+    sum += foo0(42);
+}
+''')
+
+
+if __name__ == '__main__':
+    regenerate_sources()
