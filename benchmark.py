c81b8e59b027c77f05cbf44171fc29ef87002132
Per Nordlöw <per.nordlow@gmail.com>
2018-11-10 23:56:35 +0100

    *** empty log message ***

--- benchmark.py
+++ benchmark.py
@@ -95,18 +95,24 @@ if __name__ == '__main__':
     C_CLANG_FLAGS = C_FLAGS + ['-fno-color-diagnostics', '-fno-caret-diagnostics', '-fno-diagnostics-show-option']
 
     # TODO don't regenerate sources
+
+    # C Clang
     span_C_Clang_7 = generate(f_count=f_count, language="C", args=['clang-7'] + C_FLAGS + ['-fno-color-diagnostics', '-fno-caret-diagnostics', '-fno-diagnostics-show-option'])
 
+    # C GCC
     span_Cxx_GCC_5 = generate(f_count=f_count, language="C++", args=['g++-8'] + C_FLAGS)
 
+    # C GCC
     span_C_GCC_8 = generate(f_count=f_count, language="C", args=['gcc-8'] + C_FLAGS)
     span_C_GCC_7 = generate(f_count=f_count, language="C", args=['gcc-7'] + C_FLAGS)
     span_C_GCC_6 = generate(f_count=f_count, language="C", args=['gcc-6'] + C_FLAGS)
     span_C_GCC_5 = generate(f_count=f_count, language="C", args=['gcc-5'] + C_FLAGS)
 
+    # D
     span_D_DMD = generate(f_count=f_count, language="D", args=['dmd', '-o-'])
     span_D_LDC = generate(f_count=f_count, language="D", args=['ldmd2', '-o-'])
 
+    # Rust
     span_Rust = generate(f_count=f_count, language="Rust", args=['rustc', '--crate-type', 'lib', '--emit=mir', '-o', '/dev/null', '--test'])
 
     print("D/C speedup:", span_C_GCC_8 / span_D_LDC)
