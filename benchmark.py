14d1d215fcded9e27a534de8bc584f8f89ba826d
Per Nordlöw <per.nordlow@gmail.com>
2018-11-10 22:55:02 +0100

    Fix

--- benchmark.py
+++ benchmark.py
@@ -58,8 +58,12 @@ def generate(f_count, language, args, root_path='generated'):
 
 if __name__ == '__main__':
     f_count = 50000
+
     C_FLAGS = ['-fsyntax-only']
     C_CLANG_FLAGS = C_FLAGS + ['-fno-color-diagnostics', '-fno-caret-diagnostics', '-fno-diagnostics-show-option']
+
     generate(f_count=f_count, language="C", args=['clang-7'] + C_FLAGS + ['-fno-color-diagnostics', '-fno-caret-diagnostics', '-fno-diagnostics-show-option'])
     generate(f_count=f_count, language="C", args=['gcc-8'] + C_FLAGS)
+
     generate(f_count=f_count, language="D", args=['dmd', '-o-'])
+    generate(f_count=f_count, language="D", args=['ldmd2', '-o-'])
