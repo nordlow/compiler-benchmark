495e17150206a89aa8ef4373ec7b11918fa7f11a
Per Nordlöw <per.nordlow@gmail.com>
2020-08-22 00:19:18 +0200

    *** empty log message ***

---
+++ install-dmd.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+MAJOR_VERSION="2.093.1"
+VERSION="${MAJOR_VERSION}"
+
+UNAME=$(uname -i)
+if [[ "${UNAME}" == "i386" ]]; then
+    ARCH=x86
+elif [[ "${UNAME}" == "x86_64" ]]; then
+    ARCH=amd64
+else
+    echo "Unsupported architecture ${UNAME}"
+    exit 1
+fi
+
+PNAME="dmd_${VERSION}-0_${ARCH}.deb"
+TDIR=/tmp
+PPATH="${TDIR}/${PNAME}"
+
+on_exit() {
+    rm -f ${PPATH}
+}
+trap on_exit EXIT
+
+# get it
+wget -O "${PPATH}" "http://downloads.dlang.org/releases/2.x/${MAJOR_VERSION}/${PNAME}" && \
+    exec sudo dpkg -i "${PPATH}"
