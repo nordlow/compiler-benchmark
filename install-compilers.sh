b895195077eb4e01017ad0e628ba9cceb28efd68
Per Nordlöw <per.nordlow@gmail.com>
2020-08-22 00:10:46 +0200

    *** empty log message ***

---
+++ install-compilers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# C
+sudo apt install gcc-10
+sudo apt install clang-10
+
+# C++
+sudo apt install g++-10
+sudo apt install clangxx-10
+
+# Go
+sudo apt install golang-1.14
+
+# Julia
+sudo apt install julia
