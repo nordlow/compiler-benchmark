6cba1f47ca2e4b2ebba5dd70aa1bce59e75ddb30
Per Nordlöw <per.nordlow@gmail.com>
2022-08-24 22:43:26 +0200

    Install v and vlib

--- install-vlang-master.sh
+++ install-vlang-master.sh
@@ -5,10 +5,12 @@ set -e
 # See: https://github.com/vlang/v#installing-v---from-source-preferred-method
 
 REPOS=~/.cache/repos
+
 mkdir -p "${REPOS}"
 pushd "${REPOS}"
 git clone --recurse-submodules https://github.com/vlang/v.git
 pushd v
 make
+cp -rf v vlib ~/.local/bin/
 popd
 popd
