ae85d6eae5b840c8563aa854d41cc07134e7f121
Per Nordlöw <per.nordlow@gmail.com>
2022-08-24 22:36:43 +0200

    *** empty log message ***

--- install-vlang-master.sh
+++ install-vlang-master.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+set -e
+
+# See: https://github.com/vlang/v#installing-v---from-source-preferred-method
+
+REPOS=~/.cache/repos
+mkdir -p "${REPOS}"
+pushd "${REPOS}"
+git clone --recurse-submodules https://github.com/vlang/v.git
+pushd v
+make
+popd
+popd
