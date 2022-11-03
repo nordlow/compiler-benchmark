181c01b0083912b6308c1f61e1d45b8b6896ac79
Per Nordlöw <per.nordlow@gmail.com>
2022-11-03 19:27:03 +0100

    Add install-swift.sh

--- install-swift.sh
+++ install-swift.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-wget https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
+URL=https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
+TDIR="${HOME}/.local/swift"
+mkdir -p "$TDIR"
+wget --quiet --show-progress --progress=bar:force -c "$URL" -O - | tar -xz -C "$TDIR"
