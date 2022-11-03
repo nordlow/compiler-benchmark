249566b7b1f16188ea2d42066e4e6dd25e49daca
Per Nordlöw <per.nordlow@gmail.com>
2022-11-03 19:27:54 +0100

    *** empty log message ***

--- install-swift.sh
+++ install-swift.sh
@@ -5,4 +5,4 @@ set -e
 URL=https://download.swift.org/swift-5.7.1-release/ubuntu2204/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu22.04.tar.gz
 TDIR="${HOME}/.local/swift"
 mkdir -p "$TDIR"
-wget --quiet --show-progress --progress=bar:force -c "$URL" -O - | tar -xz -C "$TDIR"
+exec wget --quiet --show-progress --progress=bar:force -c "$URL" -O - | tar -xz -C "$TDIR"
