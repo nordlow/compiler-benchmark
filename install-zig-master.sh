15d1ea531cd7a613962f1ff82a849fc91ffcf04b
Per Nordlöw <per.nordlow@gmail.com>
2024-10-30 10:34:27 +0100

    Adjust installers

--- install-zig-master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION="0.10.0"
-URL=https://ziglang.org/builds/zig-linux-x86_64-${VERSION}-dev.3659+e5e6eb983.tar.xz
-TDIR="${HOME}/.local"
-
-echo "Downloading and extracting ${URL} to ${TDIR}..."
-exec wget --quiet --show-progress --progress=bar:force -c "${URL}" -O - | tar -xJ -C "${TDIR}"
